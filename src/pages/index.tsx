9a1b59135857c7d35e88c34587366aea9d16811f
Ramin <ramin.web.97@gmail.com>
2025-04-15 15:17:08 +0400

    fix antd desing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,29 @@
-import Layout from "../shared/component/Layout/index";
-import Header from "../shared/component/Header/index";
-import CategoryList from "../shared/component/CategoryList/index";
-import {Col, Row} from "antd";
-import ProductList from "../shared/component/ProductList/index";
+// import Layout from "../shared/component/Layout/index";
+// import Header from "../shared/component/Header/index";
+// import CategoryList from "../shared/component/CategoryList/index";
+import {Col, Row} from "antd/lib";
+// import ProductList from "../shared/component/ProductList/index";
 
 export default function Home() {
   return (
     <>
-      <Layout>
-          <Header size='large' text='Гарнитура Cooler Master CH331 USB' hasButton />
-          <CategoryList />
+      {/* <Layout> */}
+          {/* <Header size='large' text='Гарнитура Cooler Master CH331 USB' hasButton /> */}
+          {/* <CategoryList /> */}
           <div className='page_content'>
               <Row gutter={20}>
                   <Col xl={12} xs={24}>
-                      <Header size='small' text='Гарнитура Cooler Master CH331 USB' />
+                      {/* <Header size='small' text='Гарнитура Cooler Master CH331 USB' /> */}
                   </Col>
                   <Col xl={12} xs={24}>
-                      <Header size='small' text='Гарнитура Cooler Master CH331 USB' />
+                      {/* <Header size='small' text='Гарнитура Cooler Master CH331 USB' /> */}
                   </Col>
               </Row>
           </div>
-          <div className='page_content'>
-              <ProductList />
-          </div>
-      </Layout>
+          {/* <div className='page_content'> */}
+              {/* <ProductList /> */}
+          {/* </div> */}
+      {/* </Layout> */}
     </>
   );
 }
